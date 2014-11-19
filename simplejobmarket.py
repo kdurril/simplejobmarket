936e218eb7e372eb4112facb2c62b228bb07111f
kenneth durril <kdurril@umd.edu>
2014-11-19 12:48:52 -0500

    fixing joins

--- simplejobmarket.py
+++ simplejobmarket.py
@@ -9,55 +9,8 @@ db = SQLAlchemy(app)
 
 
 
-class Positions(db.Model):
-    __tablename__ = 'positions'
-    __table_args__ = {"schema":"jobmarket"}
-    position_id  = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    work_group = db.Column(db.String(120))
-    position_type = db.Column(db.String(120))
-    course = db.Column(db.String(120))
-    program_min = db.Column(db.String(120))
-    program_std = db.Column(db.String(120))
-    position_overview = db.Column(db.String(120))
-    primary_duties = db.Column(db.String(120))
-    necessary_skill = db.Column(db.String(120))
-    preferred_skill = db.Column(db.String(120))
-    date_open = db.Column(db.String(120))
-    date_closed = db.Column(db.String(120))
-    available = db.Column(db.String(120))
-    supervisor_id = db.Column(db.Integer, db.ForeignKey('Supervisors.supervisor_id'), nullable=False)
-    supervisor = db.relationship("Supervisors", primaryjoin=supervisor_id == Supervisors.supervisor_id, viewonly=True)
-    positionapp = db.relationship('PositionApps', backref='Positions')
-
-    def __init__(self, position_id=None,
-                title=None, work_group=None, position_type=None,
-                course=None, program_min=None, program_std=None,
-                position_overview=None, primary_duties=None,
-                necessary_skill=None, preferred_skill=None,
-                date_open=None, date_closed=None,
-                available=None, supervisor_id=None):
-        self.position_id = position_id
-        self.title = title
-        self.work_group = work_group
-        self.position_type = position_type
-        self.course = course
-        self.program_min = program_min
-        self.program_std = program_std
-        self.position_overview = position_overview
-        self.primary_duties = primary_duties
-        self.necessary_skill = necessary_skill
-        self.preferred_skill = preferred_skill
-        self.date_open = date_open
-        self.date_closed = date_closed
-        self.available = available
-        self.supervisor_id = supervisor_id
-            
-    def __repr__(self):
-        return '<Position {0}>'.format(self.position_id)
-
 class Students(db.Model):
-    __tablename__ = 'students'
+    __tablename__ = 'Students'
     __table_args__ = {"schema":"jobmarket"}
     
     student_uid = db.Column(db.String(120), primary_key=True)
@@ -73,6 +26,8 @@ class Students(db.Model):
     credit_spring =  db.Column(db.Integer)
     request201408 = db.Column(db.String(120))
     request201501 = db.Column(db.String(120))
+    position_apps = db.relationship("PositionApps", backref='Students',
+                                lazy='dynamic')
     
 
     def __init__(self, student_uid=None, name_last=None,
@@ -110,13 +65,13 @@ class Supervisors(db.Model):
     email = db.Column(db.String(120))
     room = db.Column(db.String(120))
     center = db.Column(db.String(120))
-    position = db.relationship('Positions', 
-        backref=db.backref('Supervisors', lazy='joined'), lazy='dynamic')
+    position = db.relationship("Positions", backref='Supervisors',
+                                lazy='dynamic')
 
     def __init__(self, supervisor_id=None, 
                 name_last=None, name_first=None,
                 phone=None, email=None,
-                room=None, center=None, position=None):
+                room=None, center=None):
         self.supervisor_id = supervisor_id
         self.name_last = name_last
         self.name_frst = name_first
@@ -124,27 +79,69 @@ class Supervisors(db.Model):
         self.email = email
         self.room = room
         self.center = center
-        self.position = position
+        
 
     def __repr__(self):
         return '<Supservisor {0}>'.format(self.supervisor_id)
 
+class Positions(db.Model):
+    __tablename__ = 'positions'
+    __table_args__ = {"schema":"jobmarket"}
+    position_id  = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    work_group = db.Column(db.String(120))
+    position_type = db.Column(db.String(120))
+    course = db.Column(db.String(120))
+    program_min = db.Column(db.String(120))
+    program_std = db.Column(db.String(120))
+    position_overview = db.Column(db.String(120))
+    primary_duties = db.Column(db.String(120))
+    necessary_skill = db.Column(db.String(120))
+    preferred_skill = db.Column(db.String(120))
+    date_open = db.Column(db.String(120))
+    date_closed = db.Column(db.String(120))
+    available = db.Column(db.String(120))
+    supervisor_id = db.Column(db.Integer, db.ForeignKey('Supervisors.supervisor_id'), nullable=False)
+    supervisor = db.relationship("Supervisors", primaryjoin=supervisor_id == Supervisors.supervisor_id, viewonly=True)
+    position_app = db.relationship('PositionApps', backref='Positions')
 
+    def __init__(self, position_id=None,
+                title=None, work_group=None, position_type=None,
+                course=None, program_min=None, program_std=None,
+                position_overview=None, primary_duties=None,
+                necessary_skill=None, preferred_skill=None,
+                date_open=None, date_closed=None,
+                available=None, supervisor_id=None):
+        self.position_id = position_id
+        self.title = title
+        self.work_group = work_group
+        self.position_type = position_type
+        self.course = course
+        self.program_min = program_min
+        self.program_std = program_std
+        self.position_overview = position_overview
+        self.primary_duties = primary_duties
+        self.necessary_skill = necessary_skill
+        self.preferred_skill = preferred_skill
+        self.date_open = date_open
+        self.date_closed = date_closed
+        self.available = available
+        self.supervisor_id = supervisor_id
+            
+    def __repr__(self):
+        return '<Position {0}>'.format(self.position_id)
 
-#positionApps = db.Table('positionApps',
-#    db.Column('app_id', db.Integer, primary_key=True),
-#    db.Column('student_uid', db.Integer, db.ForeignKey('students.student_uid')),
-#    db.Column('position_id', db.Integer, db.ForeignKey('positions.position_id'))
-#)
 
 class PositionApps(db.Model):
-    __tablename__ = 'positionapps'
+    __tablename__ = 'PositionApps'
     __table_args__ = {"schema":"jobmarket"}
     app_id = db.Column(db.Integer)
     position_id = db.Column(db.Integer, db.ForeignKey('Positions.position_id'), primary_key=True)
     student_uid = db.Column(db.String(120), db.ForeignKey('Students.student_uid'), primary_key=True)
     students = db.relationship('Students', primaryjoin=student_uid == Students.student_uid)
-    offers = db.relationship('Offers', backref='PositionApps')
+    positions = db.relationship('Positions', secondaryjoin=position_id == Positions.position_id)
+    offers = db.relationship('Offers', backref='PositionApps',
+                                lazy='dynamic')
     db.UniqueConstraint('position_id', 'student_uid', name='unique_app')
     #students = db.relationship('Students') #, backref="positionapps"
     #offers = db.relationship('Offers') 
@@ -169,13 +166,13 @@ class Offers(db.Model):
     __tablename__ = 'offers'
     __table_args__ = {"schema":"jobmarket"}
     offer_id = db.Column(db.Integer, primary_key=True)
-    app_id = db.Column(db.Integer, db.ForeignKey('positionapps.app_id'))
+    app_id = db.Column(db.Integer, db.ForeignKey('PositionApps.app_id'))
     offer_made = db.Column(db.String(120))
     offer_date = db.Column(db.String(120))
     response = db.Column(db.String(120))
     response_date = db.Column(db.String(120))
     available = db.Column(db.String(120))
-    position_apps = db.relationship('PositionApps',primaryjoin = app_id == PositionApps.app_id)
+    position_apps = db.relationship('PositionApps',primaryjoin=app_id == PositionApps.app_id)
 
     def __init__(self, offer_id=None, app_id=None,
                 offer_made=None, offer_date=None,
